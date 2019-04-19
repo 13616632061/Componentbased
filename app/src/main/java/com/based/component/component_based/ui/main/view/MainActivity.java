3296eaca471cdaa02aa74eb38487b2da2e906c7c
13530531749@163.com <13530531749@163.com>
2019-04-19 18:07:20 +0800

    Initial commit

---
+++ app/src/main/java/com/based/component/component_based/ui/main/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.based.component.component_based.ui.main.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.based.component.component_based.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
